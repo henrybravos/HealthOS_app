b44ac6797330d7a617a5fc60e2b34778c803b6b7
Henry Bravo <hbravos.info@gmail.com>
2024-04-18 02:44:23 -0500

    feat: 🎸 implement drawer screens

--- App.tsx
+++ App.tsx
@@ -1,25 +1,56 @@
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import { NavigationContainer } from '@react-navigation/native'
 import { StatusBar } from 'expo-status-bar'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import { Provider } from 'react-native-paper'
+import { View } from 'react-native'
+import { Provider, useTheme } from 'react-native-paper'
 
-import FirebaseAppProvider from '@context/FirebaseProvider'
+import { FirebaseAppProvider } from '@common/context'
 
+import { CreateRacsManagement, ListRacsManagement } from '@features/racs'
+
+const Drawer = createDrawerNavigator()
+
+function Navigation() {
+  const theme = useTheme()
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: theme.colors.surfaceVariant,
+          },
+        }}
+      >
+        <Drawer.Screen
+          name="ListRacs"
+          component={ListRacsManagement}
+          options={{
+            headerTitleStyle: { fontSize: 14 },
+            title: 'Lista de RACS',
+          }}
+        />
+        <Drawer.Screen
+          name="CreateRacs"
+          component={CreateRacsManagement}
+          options={{
+            headerTitleStyle: { fontSize: 14 },
+            title:
+              'REPORTE DE ACTOS, CONDICIONES, INCIDENTES, SUGERENCIAS Y BUENAS PRÁCTICAS ( RACS)',
+          }}
+        />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  )
+}
 export default function App() {
   return (
     <Provider>
       <FirebaseAppProvider>
-        <SafeAreaView style={styles.container}>
+        <View style={{ flex: 1 }}>
           <StatusBar style="auto" />
-        </SafeAreaView>
+          <Navigation />
+        </View>
       </FirebaseAppProvider>
     </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 40,
-    margin: 8,
-  },
-})
