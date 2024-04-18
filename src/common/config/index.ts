ac5863ae8cfd99d366a46e3b7254f8e617a6283e
Henry Bravo <hbravos.info@gmail.com>
2024-04-18 02:47:06 -0500

    refactor: 💡 implement config firebase

---
+++ src/common/config/index.ts
@@ -0,0 +1,1 @@
+export { default as appFirebase } from './firebaseConfig'
---
+++ src/common/const/collections.ts
@@ -0,0 +1,9 @@
+export const COLLECTIONS = {
+  companies: 'companies',
+  eventTypes: 'event_types',
+  occupations: 'occupations',
+  places: 'places',
+  unsafeActs: 'unsafe_acts',
+  racs: 'racs',
+  unsafeConditions: 'unsafe_conditions',
+}
