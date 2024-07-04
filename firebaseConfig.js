bf852a0359e0ac779a67c6fe10ba8b04bca3d263
Henry Bravo <hbravos.info@gmail.com>
2024-07-04 07:38:18 -0500

    remove keys project

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,13 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCZijf4WCOYgv-7x_Tu715RkPuMTtRQMkc',
-  authDomain: 'noatum-app.firebaseapp.com',
-  projectId: 'noatum-app',
-  storageBucket: 'noatum-app.appspot.com',
-  messagingSenderId: '470218412563',
-  appId: '1:470218412563:web:0f8b71aeafc7be9e3225e6',
-  measurementId: 'G-FNVD1F1MZC',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 }
 
 const app = initializeApp(firebaseConfig)
